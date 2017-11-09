ceaf5265c01a3d34cdb02a8731f02402e9179952
Elliot Winkler <elliot.winkler@gmail.com>
2017-11-08 21:43:37 -0600

    Give users/devs more insight into complex matchers
    All of the validation matchers ultimately depend on allow_value to do
    their work. When such a matcher fails, it can often be frustrating to
    pin down the cause, because we don't know what the matcher is doing
    under the hood.
    
    This commit changes the error messages for all of the validation
    matchers so that they list all the assertions they make and whether they
    passed or failed.
    
    It also implements #pretty_print for a bunch of internal classes so that
    when developers use Pry to debug issues they can see into objects
    without being overloaded with information they don't need.

--- lib/shoulda/matchers/util.rb
+++ lib/shoulda/matchers/util.rb
@@ -52,6 +52,22 @@ module Shoulda
         "#{inspect_value(range.first)} to #{inspect_value(range.last)}"
       end
 
+      def self.pretty_print(object, pp, properties)
+        pp.object_address_group(object) do
+          pp.breakable(' ')
+          pp.seplist(properties, nil, :each_pair) do |key, value|
+            pp.group do
+              pp.text(key.to_s)
+              pp.text(':')
+              pp.group(1) do
+                pp.breakable(' ')
+                pp.pp(value)
+              end
+            end
+          end
+        end
+      end
+
       def self.dummy_value_for(column_type, array: false)
         if array
           [dummy_value_for(column_type, array: false)]
